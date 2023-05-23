85643e1387ee9f78c7d12a6059ce454b14ce9055
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-05-22 17:32:26 -0700

    modefied index.js

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function Car (make,model,year){
-this.make= make;
-this.model= model;
-this.year= year;
-
+// Define the Car constructor function
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
 }
 
-Car.prototype.getDescription =function(){
-console.log(`Car: ${this.make} ${this.model} ${this.year}`);
+// Add the getDescription method to the Car prototype
+Car.prototype.getDescription = function() {
+  console.log(`Car: ${this.make} Model:${this.model} Year: ${this.year}`);
+};
 
+// Define the ElectricCar constructor function as a subclass of Car
+function ElectricCar(make, model, year, range) {
+  // Call the Car constructor to set inherited properties
+  Car.call(this, make, model, year);
+  this.range = range; // Add the range property specific to ElectricCar
 }
 
-function ElectricCAr(make,model,year,range ){
-Car.call(this, make,model,year);
-this.range= range;
-}
+// Set up the prototype chain for ElectricCar
+Object.setPrototypeOf(ElectricCar.prototype, Car.prototype);
+
+// Override the getDescription method of ElectricCar
+ElectricCar.prototype.getDescription = function() {
+  console.log(`Car: ${this.make}  Model:${this.model}  Year: ${this.year}  Range:${this.range}`);
+};
+
+// Create an instance of ElectricCar
+const myCar = new ElectricCar("tesla", "S", 2019, "300");
+
+// Call the getDescription method on the instance
+myCar.getDescription();
